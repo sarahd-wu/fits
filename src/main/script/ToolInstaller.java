0840a2485e11c85344dbb7c45d5a69b329172b0b
Sarah Wu <sarahd.wu6@gmail.com>
2026-02-05 12:37:49 +0600

    fixed issue with dir naming. resolved exiftool testing

--- src/main/script/ToolInstaller.java
+++ src/main/script/ToolInstaller.java
@@ -256,6 +256,14 @@ public class ToolInstaller {
         var archive = downloadAndVerify(WINDOWS);
         var targetDir = toolDir.resolve(tool.windowsDir);
         extractZip(archive, targetDir);
+        targetDir = targetDir.resolve("/exiftool-13.49_64");
+    
+        // Debugging
+        // System.out.println("archive: " + archive.toAbsolutePath() + " targetDir: " + targetDir.toAbsolutePath());
+        // try (var stream = Files.list(targetDir)) {
+        //     stream.forEach(p -> System.out.println(p.getFileName()));
+        // }
+
         Files.move(targetDir.resolve("exiftool(-k).exe"), targetDir.resolve("exiftool.exe"));
         Files.deleteIfExists(archive);
     }
